7d239a628c99c86a0729cc3a79d30fa746003563
1583734440 <45280616+1583734440@users.noreply.github.com>
2020-11-24 15:22:02 +0800

    创建订单功能实现

--- src/main/java/com/jiuzhang/seckill/service/SeckillActivityService.java
+++ src/main/java/com/jiuzhang/seckill/service/SeckillActivityService.java
@@ -1,6 +1,13 @@
 package com.jiuzhang.seckill.service;
 
+import com.alibaba.fastjson.JSON;
+import com.jiuzhang.seckill.db.dao.OrderDao;
+import com.jiuzhang.seckill.db.dao.SeckillActivityDao;
+import com.jiuzhang.seckill.db.po.Order;
+import com.jiuzhang.seckill.db.po.SeckillActivity;
+import com.jiuzhang.seckill.mq.RocketMQService;
 import com.jiuzhang.seckill.util.RedisService;
+import com.jiuzhang.seckill.util.SnowFlake;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +16,47 @@ public class SeckillActivityService {
 
     @Autowired
     private RedisService redisService;
+
+    @Autowired
+    private SeckillActivityDao seckillActivityDao;
+
+    @Autowired
+    private RocketMQService rocketMQService;
+
+    /**
+     * datacenterId;  数据中心
+     * machineId;     机器标识
+     * 在分布式环境中可以从机器配置上读取
+     * 单机开发环境中先写死
+     */
+    private SnowFlake snowFlake = new SnowFlake(1, 1);
+    
+    /**
+     * 创建订单
+     *
+     * @param seckillActivityId
+     * @param userId
+     * @return
+     * @throws Exception
+     */
+    public Order createOrder(long seckillActivityId, long userId) throws Exception {
+        /*
+         * 1.创建订单
+         */
+        SeckillActivity seckillActivity = seckillActivityDao.querySeckillActivityById(seckillActivityId);
+        Order order = new Order();
+        //采用雪花算法生成订单ID
+        order.setOrderNo(String.valueOf(snowFlake.nextId()));
+        order.setSeckillActivityId(seckillActivity.getId());
+        order.setUserId(userId);
+        order.setOrderAmount(seckillActivity.getSeckillPrice().longValue());
+        /*
+         *2.发送创建订单消息
+         */
+        rocketMQService.sendMessage("seckill_order", JSON.toJSONString(order));
+
+        return order;
+    }
 
     /**
      * 判断商品是否还有库存
@@ -19,4 +67,6 @@ public class SeckillActivityService {
         String key = "stock:" + activityId;
         return redisService.stockDeductValidator(key);
     }
+
+
 }
